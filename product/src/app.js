d28d4fcc0d30e1f6729836748428d77f2542929e
Rodolfo Henrique Silva <rodolfo.silva@HMB.LOCAL>
2020-11-16 16:59:24 -0300

    Configurando limite json e CORS

--- product/src/app.js
+++ product/src/app.js
@@ -26,7 +26,20 @@ const productRoute = require("./routes/product-route");
 const customerRoute = require("./routes/customer-route");
 const orderRoute = require("./routes/order-route");
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "5mb",
+  })
+);
+
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
 app.use("/", indexRoute);
 app.use("/products", productRoute);
